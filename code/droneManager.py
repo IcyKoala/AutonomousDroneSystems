4d67f4d6a9b5f937d94a9ac9b224f791f8e84f91
IcyKoala <90623742+IcyKoala@users.noreply.github.com>
2024-05-16 13:43:26 +0200

    Basic setup for setup selection
    *very* basic setup for controlmode & pattern select
    Added setup for manual control

--- code/droneManager.py
+++ code/droneManager.py
@@ -1,18 +1,64 @@
+from Drone import DroneController
+from PathPlanning import PathPlanning
+from enum import Enum
 
-class droneManager:
+class DroneManager:
+    
     def __init__(self) -> None:
-        pass
+        self.controller = DroneController()
+        self.pathPlanner = PathPlanning()
+        self.droneList = []
+        self.patterns = Enum('Patterns', ['DONUT'])
+        self.controlSelect()  # Call controlSelect method here
 
-    def checkDrones():
-        #function to check the drones
-        cameraController.checkDrone()
-        pass
+    def fetchPaths(self):
+        for drone in self.droneList:
+            self.pathPlanner.calcPath(drone) 
 
-    def moveDrone(drone): #dis a thread
-        pathplanning.findpath(drone)
-        #function to move the drone
-        if droneType == physical:
-            droneController.move()
-        else:
-            unityController.move()
-        pass
+    def controlSelect(self):
+        controlMode = input("Select operation mode: \n 1 - Manual \n 2 - Automatic\n Selected: ")
+        validInput = False
+        while not validInput:
+            if controlMode == "1":
+                validInput = True
+                self.controller.manualControl()
+            elif controlMode == "2":
+                validInput = False
+                self.patternSelect()
+            else:
+                input("incorrect input, please try again")
+
+    def patternSelect(self):
+        controlMode = input("Select Patterd: \n 1 - Donut \n Selected: ")
+        validInput = False
+        while not validInput:
+            if controlMode == "1":
+                validInput = True
+                self.instructDrones(self.patterns.DONUT)
+            else:
+                input("incorrect input, please try again: ")
+
+    def instructDrones(self):
+        finishedPattern = False
+        while not finishedPattern:
+            self.fetchPaths()
+
+# Create an instance of DroneManager
+manager = DroneManager()
+
+
+
+
+# def checkDrones():
+#         #function to check the drones
+#         cameraController.checkDrone()
+#         pass
+
+#     def moveDrone(drone): #dis a thread
+#         pathplanning.findpath(drone)
+#         #function to move the drone
+#         if droneType == physical:
+#             droneController.move()
+#         else:
+#             unityController.move()
+#         pass
--- code/unityController.py
+++ code/unityController.py
@@ -1,4 +1,4 @@
-class unityController:
+class UnityController:
     def __init__(self) -> None:
        pass
 
