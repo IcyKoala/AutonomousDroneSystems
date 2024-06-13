64f2c2874f56f81e66bc2191aa08c1f0e978fdb4
Ryan <keemink.ryan@gmail.com>
2024-06-13 12:43:31 +0200

    renamed file to findports

--- code/findport.py
+++ code/findport.py
@@ -1,25 +1,22 @@
 import cflib.crtp
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
-from cflib.positioning.motion_commander import MotionCommander
-from camera import CameraDetector
-import math
-import cv2
-import time
-import threading
+
 
 manualMovementDistance = 0.1
 redURI = 'radio://0/80/2M/E7E7E7E7E7'
 p1 = "radio://0/"
 p2 = "/2M/E7E7E7E7E7"
 cflib.crtp.init_drivers(enable_debug_driver=False)
+test = []
 
-for i in range (100):
-    if i == 80:
-        continue
+for i in range (125):
     try:
         with SyncCrazyflie(p1 + str(i) + p2) as scf:
-            print(i)
+            print("connected")
+            test +=[i]
             break
 
     except:
         pass
+
+print(test)
